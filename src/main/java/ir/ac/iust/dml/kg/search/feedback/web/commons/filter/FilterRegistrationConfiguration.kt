617249c94e3c05009b4cac8846c11f0a09e471be
Majid Asgari <majid.asgari@gmail.com>
2017-09-23 15:37:30 +0330

    add copyright texts

--- src/main/java/ir/ac/iust/dml/kg/search/feedback/web/commons/filter/FilterRegistrationConfiguration.kt
+++ src/main/java/ir/ac/iust/dml/kg/search/feedback/web/commons/filter/FilterRegistrationConfiguration.kt
@@ -1,3 +1,9 @@
+/*
+ * Farsi Knowledge Graph Project
+ * Iran University of Science and Technology (Year 2017)
+ * Developed by Majid Asgari.
+ */
+
 package ir.ac.iust.dml.kg.search.feedback.web.commons.filter
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean
